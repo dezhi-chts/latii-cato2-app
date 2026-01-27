77adf783750f1737af8d501d7c0dcb8c1bd971f2
wangyuncong <wangyuncong9@gmail.com>
2026-01-27 09:01:58 +0800

    temp commit

--- src/app/brand-editor/components/body-components/parameter-option.tsx
+++ src/app/brand-editor/components/body-components/parameter-option.tsx
@@ -1,6 +1,46 @@
 "use client";
 
-const ParameterOption = () => {
+import { useState, useEffect } from "react";
+import {
+	fetchCompanyByKeycloakUser
+} from "@/services/companyService";
+import {
+	fetchProductAttributeVersionByAttributeNameCompanyId,
+	fetchUnitAttributesWithOptionsByVersionId
+} from "@/services/profileEditorService";
+
+
+const LibraryOption = () => {
+
+	const [companyMsg, setCompanyMsg] = useState<Record<string, any>>({});
+	const [unitMsg, setUnitMsg] = useState<Record<string, any>>({});
+
+	useEffect(() => {
+		initData()
+	}, []);
+
+	const initData = async () => {
+		const companyRes = await fetchCompanyByKeycloakUser();
+		if (companyRes.status == "success") {
+			setCompanyMsg(companyRes?.data)
+			const [
+				unitVersion
+			] = await Promise.all([
+				fetchProductAttributeVersionByAttributeNameCompanyId(
+					"UNIT Attribute Tree",
+					companyRes?.data.id
+				)
+			]);
+			if (unitVersion?.status == "success" && Array.isArray(unitVersion?.data) && unitVersion?.data.length != 0) {
+				const [
+					unitAttributesWithOptions
+				] = await Promise.all([
+					fetchUnitAttributesWithOptionsByVersionId(unitVersion?.data[0]?.id)
+				]);
+				setUnitMsg(unitAttributesWithOptions?.data);
+			}
+		}
+	};
 
 	return (
 		<div>
@@ -9,4 +49,4 @@ const ParameterOption = () => {
 	);
 };
 
-export default ParameterOption;
+export default LibraryOption;
