35cfb7a8100b49ffad7361973c2b1d6cb12d3ff9
florandres-latii <flor@latii.com>
2026-02-18 19:27:30 +0100

    Removed not used tabs

--- src/app/brand-editor/components/body-components/settings.tsx
+++ src/app/brand-editor/components/body-components/settings.tsx
@@ -3,6 +3,7 @@
 import { useState } from "react";
 import ProjectsSettings from "./settings-components/Projects-settings";
 import QuoteSettings from "./settings-components/Quote-settings";
+import BoxesType from "./settings-components/ Boxes-Type";
 
 const Settings = () => {
   const tabs = [
@@ -12,11 +13,7 @@ const Settings = () => {
     },
     {
       id: 2,
-      text: "Quotes",
-    },
-    {
-      id: 3,
-      text: "Items",
+      text: "Boxes Type",
     },
   ];
 
@@ -49,7 +46,7 @@ const Settings = () => {
       </div>
       <div className="pl-6 pt-10">
         {selectedTab.id === 1 && <ProjectsSettings />}
-        {selectedTab.id === 2 && <QuoteSettings />}
+        {selectedTab.id === 2 && <BoxesType />}
       </div>
     </div>
   );
