63e8c10dad3b82ff6b8fa61487fd6aa29202c807
miaoxingqiuqiu0020 <guona0020@gmail.com>
2026-02-25 11:59:42 +0800

    Update PdfWrapper buttons

--- src/app/projects/[projectId]/takeoff/[takeoffId]/components/pdf/PdfWrapper.tsx
+++ src/app/projects/[projectId]/takeoff/[takeoffId]/components/pdf/PdfWrapper.tsx
@@ -117,13 +117,17 @@ const getResizeCursorStyle = (pointType: string) => {
 
 // 以下的框类型显示 读取按钮
 const showReadBtnGroupTypes = [GroupType.OCR];
+
 // 以下的框类型显示 确认按钮
 const showConfirmBtnGroupTypes = [GroupType.DrawingIndex, GroupType.TitleInfo];
 
-// 以下的框类型显示 数字按钮,复制按钮
+// 以下的框类型显示 数字按钮
 const showNumBtnGroupTypes = [GroupType.Item, GroupType.WindowDoorUnitList];
 
-// 以下框类型显示下拉框
+// 以下框类型显示  复制按钮集合
+const showCopyBtnGroupTypes = [GroupType.WindowDoorUnitList, GroupType.Item, GroupType.LayerInfo, GroupType.Description];
+
+// 以下框类型显示类型下拉框
 const showSelectGroupTypes = [GroupType.FloorPlan, GroupType.Elevation, GroupType.WindowDoorUnit, GroupType.Table, GroupType.KeyNotes];
 
 // 框类型对应的颜色
@@ -2461,66 +2465,68 @@ const PdfWrapper = forwardRef(
                           </Popconfirm>
                         </div>
                       </div>
-                      {showNumBtnGroupTypes.includes(type) &&
+                      {showCopyBtnGroupTypes.includes(type) &&
                         <div
-                          className="transition-all flex justify-center items-center gap-1"
+                          className="transition-all"
                           style={{
                             position: "absolute",
-                            left: width / 2 - 30 + "px",
+                            left: width / 2 - 50 + "px",
                             top:
                               maxY > stageHeight - 10
                                 ? height - 30 + "px"
                                 : height + 2 + "px",
                           }}
                         >
-                          <Popover placement="bottom"
-                            title={null}
-                            content={<div className="text-xs text-basicGray">Extend to Previous Page</div>}
-                            trigger="hover"
-                          >
-                            <div className="w-[30px] h-[20px] flex justify-center items-center text-white rounded-tl-md rounded-bl-md cursor-pointer" style={{ backgroundColor: color }}
-                              onClick={() => {
-                                handleCreateBox(item, 'prev');
-                              }}>
-                              <span className="-mt-[2px] text-xs">{'<'}</span>
-                            </div>
-                          </Popover>
-
-                          <div className="w-[28px] h-[20px] flex justify-center items-center text-white cursor-pointer" style={{ backgroundColor: color }}
-                            onClick={() => {
-                              handleCreateBox(item, 'center');
-                            }}>
-                            <Image src="/assets/icons/layers-linked.svg" alt="layers-linked icon" width={15} height={15} preview={false}></Image>
-                          </div>
-
-                          <Popover placement="bottom"
-                            title={null}
-                            content={<div className="text-xs text-basicGray">Extend to Next Page</div>}
-                            trigger="hover"
-                          >
-                            <div className="w-[30px] h-[20px] flex justify-center items-center text-white rounded-tr-md rounded-br-md cursor-pointer" style={{ backgroundColor: color }}
-                              onClick={() => {
-                                handleCreateBox(item, 'next');
-                              }}>
-                              <span className="-mt-[2px] text-xs">{'>'}</span>
-                            </div>
-                          </Popover>
-
-                          <div className="w-[20px] h-[20px] flex justify-center items-center text-white rounded-full cursor-pointer" style={{ backgroundColor: color }} onClick={() => {
-                          }}>
-                            <Popover placement="rightBottom"
-                              title={<div className="text-xs font-medium">Chain Link</div>}
-                              content={<div className="w-[300px] text-xs text-basicGray">
-                                Create Linked Box Create a new box linked to this item.
-                                <ul className="ml-3 list-disc">
-                                  <li>Use the <span className="text-black font-medium">Center button</span> to create a box on the current page.</li>
-                                  <li>Use the <span className="text-black font-medium">Previous or Next buttons</span> to create a linked box with this same label on the adjacent pages.</li>
-                                </ul>
-                              </div>}
+                          <div className="flex justify-center items-center gap-1">
+                            <Popover placement="bottom"
+                              title={null}
+                              content={<div className="text-xs text-basicGray">Extend to Previous Page</div>}
                               trigger="hover"
                             >
-                              <Image src="/assets/icons/info-white.svg" alt="plus icon" width={12} height={12} preview={false}></Image>
+                              <div className="w-[30px] h-[20px] flex justify-center items-center text-white rounded-tl-md rounded-bl-md cursor-pointer" style={{ backgroundColor: color }}
+                                onClick={() => {
+                                  handleCreateBox(item, 'prev');
+                                }}>
+                                <span className="-mt-[2px] text-xs">{'<'}</span>
+                              </div>
                             </Popover>
+
+                            <div className="w-[28px] h-[20px] flex justify-center items-center text-white cursor-pointer" style={{ backgroundColor: color }}
+                              onClick={() => {
+                                handleCreateBox(item, 'center');
+                              }}>
+                              <Image src="/assets/icons/layers-linked.svg" alt="layers-linked icon" width={15} height={15} preview={false}></Image>
+                            </div>
+
+                            <Popover placement="bottom"
+                              title={null}
+                              content={<div className="text-xs text-basicGray">Extend to Next Page</div>}
+                              trigger="hover"
+                            >
+                              <div className="w-[30px] h-[20px] flex justify-center items-center text-white rounded-tr-md rounded-br-md cursor-pointer" style={{ backgroundColor: color }}
+                                onClick={() => {
+                                  handleCreateBox(item, 'next');
+                                }}>
+                                <span className="-mt-[2px] text-xs">{'>'}</span>
+                              </div>
+                            </Popover>
+
+                            <div className="w-[20px] h-[20px] flex justify-center items-center text-white rounded-full cursor-pointer" style={{ backgroundColor: color }} onClick={() => {
+                            }}>
+                              <Popover placement="rightBottom"
+                                title={<div className="text-xs font-medium">Chain Link</div>}
+                                content={<div className="w-[300px] text-xs text-basicGray">
+                                  Create Linked Box Create a new box linked to this item.
+                                  <ul className="ml-3 list-disc">
+                                    <li>Use the <span className="text-black font-medium">Center button</span> to create a box on the current page.</li>
+                                    <li>Use the <span className="text-black font-medium">Previous or Next buttons</span> to create a linked box with this same label on the adjacent pages.</li>
+                                  </ul>
+                                </div>}
+                                trigger="hover"
+                              >
+                                <Image src="/assets/icons/info-white.svg" alt="plus icon" width={12} height={12} preview={false}></Image>
+                              </Popover>
+                            </div>
                           </div>
                         </div>
                       }
