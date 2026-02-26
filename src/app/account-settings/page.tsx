14177930506e37ebce04ebcf8bf54b22314f14bf
Tomas Bran <tomas@latii.com>
2026-02-26 18:53:51 +0100

    unsaved page addition to tailwind refactor

--- src/app/account-settings/page.tsx
+++ src/app/account-settings/page.tsx
@@ -43,7 +43,7 @@ const AccountSettings = () => {
     <div className="mt-10 flex flex-col gap-2 w-full">
       <div className="flex flex-col gap-1 pl-12">
         <p className="text-xl text-forumBlue-normal">Account Preferences</p>
-        <p className="text-xs text-basicGray">
+        <p className="text-xs text-grey-normal">
           Manage your data and your team members, privacy and security.
         </p>
       </div>
