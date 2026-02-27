b603b8deb784cac4ecf77c0255a9cd4afc1f98e4
wangyuncong <wangyuncong9@gmail.com>
2026-02-27 09:47:50 +0800

    Annotate the entry to the brand editor

--- src/app/brand-editor/components/header.tsx
+++ src/app/brand-editor/components/header.tsx
@@ -44,7 +44,7 @@ const Header = ({ selectedOptionId, setSelectedOptionId }: HeaderProps) => {
         <p className="text-[#717171] text-lg" style={{ fontSize: "18px" }}>
           Your Company
         </p>
-        <p className="text-baseGray text-sm">
+        <p className="text-grey-light-strong text-sm">
           Save all the information of your company
         </p>
       </div>
