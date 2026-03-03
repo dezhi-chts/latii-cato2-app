31257d3a3f7987490aa857c98a4791d8a121a6aa
Tomas Bran <tomas@latii.com>
2026-03-03 19:52:48 +0100

    feat(item review page): created the table with mocked data

--- src/app/projects/[projectId]/takeoff/[takeoffId]/manual-merge/item-review/components/Body.tsx
+++ src/app/projects/[projectId]/takeoff/[takeoffId]/manual-merge/item-review/components/Body.tsx
@@ -2,9 +2,140 @@
 
 import { Divider } from "antd";
 import { BodyOptions } from "./BodyOptions";
-
 import PdfViewer from "./PdfViewer";
 import { useState } from "react";
+import { Table } from "./Table";
+
+export type Sublabel = {
+  id: string;
+  label: string;
+  category: string;
+  type: string;
+  open: string;
+};
+
+export type Label = {
+  id: string;
+  label: string;
+  category: string;
+  type: string;
+  open: string;
+  sublabels: Sublabel[];
+};
+
+const labels: Label[] = [
+  {
+    id: "1",
+    label: "A",
+    category: "Window",
+    type: "Casement",
+    open: "Inswing",
+    sublabels: [
+      {
+        id: "1-1",
+        label: "A1",
+        category: "Window",
+        type: "Double Casement",
+        open: "Inswing",
+      },
+      {
+        id: "1-2",
+        label: "A2",
+        category: "Door",
+        type: "Sliding",
+        open: "XO",
+      },
+    ],
+  },
+  {
+    id: "2",
+    label: "B",
+    category: "System",
+    type: "-",
+    open: "-",
+    sublabels: [
+      {
+        id: "2-1",
+        label: "B1",
+        category: "Window",
+        type: "Awning",
+        open: "Outswing",
+      },
+    ],
+  },
+  {
+    id: "3",
+    label: "C",
+    category: "Door",
+    type: "Single Swing",
+    open: "Outswing",
+    sublabels: [],
+  },
+  {
+    id: "4",
+    label: "D",
+    category: "System",
+    type: "-",
+    open: "-",
+    sublabels: [],
+  },
+  {
+    id: "5",
+    label: "E",
+    category: "Window",
+    type: "Casement",
+    open: "Inswing",
+    sublabels: [],
+  },
+  {
+    id: "6",
+    label: "F",
+    category: "Door",
+    type: "Single Swing",
+    open: "Outswing",
+    sublabels: [],
+  },
+  {
+    id: "7",
+    label: "G",
+    category: "Window",
+    type: "Direct Set",
+    open: "-",
+    sublabels: [
+      {
+        id: "7-1",
+        label: "G1",
+        category: "Window",
+        type: "Direct Set",
+        open: "-",
+      },
+    ],
+  },
+  {
+    id: "8",
+    label: "H",
+    category: "Window",
+    type: "Direct Set",
+    open: "-",
+    sublabels: [],
+  },
+  {
+    id: "9",
+    label: "I",
+    category: "Window",
+    type: "Direct Set",
+    open: "-",
+    sublabels: [],
+  },
+  {
+    id: "10",
+    label: "J",
+    category: "Window",
+    type: "Direct Set",
+    open: "-",
+    sublabels: [],
+  },
+];
 
 type BodyProps = {
   showOptions: boolean;
@@ -31,7 +162,9 @@ export const Body = ({ showOptions }: BodyProps) => {
           />
         </div>
         <Divider type="vertical" className="bg-grey-light-hover h-auto" />
-        <p className="w-2/6">Table (WIP)</p>
+        <div className="w-2/6">
+          <Table labels={labels} />
+        </div>
       </div>
     </div>
   );
--- src/app/projects/[projectId]/takeoff/[takeoffId]/manual-merge/item-review/components/Table.tsx
+++ src/app/projects/[projectId]/takeoff/[takeoffId]/manual-merge/item-review/components/Table.tsx
@@ -1,39 +1,108 @@
-import { Input, Radio } from "antd";
+import { Radio } from "antd";
+import { Label, Sublabel } from "./Body";
 import Image from "next/image";
 
-export const Table = ({ file }: any) => {
-  return (
-    <div className="w-full mt-6">
-      <div className="w-full flex flex-col">
-        <div className="flex rounded-t-md text-basicGray bg-baseLight">
-          <div className="w-1/2 flex justify-center items-center py-3">
-            Label
-          </div>
-          <div className="w-1/2 flex justify-center items-center py-3">
-            Sub-Label
-          </div>
-        </div>
+export const Table = ({ labels }: any) => {
+  const FIXED_WIDTH = 180;
 
-        {file?.labels?.map((label: any) => (
-          <div
-            key={label.label}
-            className="flex border-b border-r border-l border-primaryN30 "
-          >
-            <div className="w-1/2 flex justify-center items-center gap-4 border-r border-primaryN30 py-2">
-              <Radio />{" "}
-              <Input value={label.label} className="w-3/5 text-center " />
+  const standardCellClass =
+    "border-r border-grey-light-hover h-full flex items-center justify-center";
+
+  return (
+    <div className="w-full mt-6 overflow-x-auto">
+      <div className="flex rounded-t-lg text-grey-normal bg-grey-light py-3 text-center min-w-max">
+        <div className="w-8 flex-shrink-0 sticky left-0 z-10 bg-grey-light px-2" />
+        <div style={{ width: FIXED_WIDTH }}>Label</div>
+        <div style={{ width: FIXED_WIDTH }}>Sub-Label</div>
+        <div style={{ width: FIXED_WIDTH }}>Category</div>
+        <div style={{ width: FIXED_WIDTH }}>Type</div>
+        <div style={{ width: FIXED_WIDTH }}>Open</div>
+        <div className="w-8 flex-shrink-0 sticky right-0 z-10 bg-grey-light px-2" />
+      </div>
+
+      <div>
+        {labels.map((label: Label) => {
+          return (
+            <div key={label.id}>
+              <div className="flex text-grey-normal text-center min-w-max h-12 items-center border-b border-grey-light-hover ">
+                <div className="w-8 flex-shrink-0 sticky left-0 h-full z-10 px-2  border-l border-r border-grey-light-hover bg-white">
+                  <Radio className="h-full" />
+                </div>
+                <div
+                  style={{ width: FIXED_WIDTH }}
+                  className={standardCellClass}
+                >
+                  {label.label}
+                </div>
+                <div
+                  style={{ width: FIXED_WIDTH }}
+                  className={standardCellClass}
+                >
+                  {label.label}
+                </div>
+                <div
+                  style={{ width: FIXED_WIDTH }}
+                  className={standardCellClass}
+                >
+                  {label.category}
+                </div>
+                <div
+                  style={{ width: FIXED_WIDTH }}
+                  className={standardCellClass}
+                >
+                  {label.type}
+                </div>
+                <div style={{ width: FIXED_WIDTH }}>{label.open}</div>
+                <div className="w-8 flex-shrink-0 sticky right-0 bg-white z-10 px-2 border-l border-r border-grey-light-hover h-full flex justify-center items-center">
+                  <Image
+                    src="/assets/icons/delete-merge-item.svg"
+                    alt="Delete"
+                    width={12}
+                    height={12}
+                  />
+                </div>
+              </div>
+              {label.sublabels.map((sublabel: Sublabel) => {
+                return (
+                  <div className="flex text-grey-normal text-center min-w-max h-12 items-center border-b border-grey-light-hover ">
+                    <div className="w-8 flex-shrink-0 sticky left-0 bg-white z-10 px-2 border-l border-r border-grey-light-hover h-full"></div>
+                    <div
+                      style={{ width: FIXED_WIDTH }}
+                      className={standardCellClass}
+                    />
+                    <div
+                      style={{ width: FIXED_WIDTH }}
+                      className={standardCellClass}
+                    >
+                      {sublabel.label}
+                    </div>
+                    <div
+                      style={{ width: FIXED_WIDTH }}
+                      className={standardCellClass}
+                    >
+                      {sublabel.category}
+                    </div>
+                    <div
+                      style={{ width: FIXED_WIDTH }}
+                      className={standardCellClass}
+                    >
+                      {sublabel.type}
+                    </div>
+                    <div style={{ width: FIXED_WIDTH }}>{sublabel.open}</div>
+                    <div className="w-8 flex-shrink-0 sticky right-0 bg-white z-10 px-2 border-l border-r border-grey-light-hover h-full flex justify-center items-center">
+                      <Image
+                        src="/assets/icons/delete-merge-item.svg"
+                        alt="Delete"
+                        width={12}
+                        height={12}
+                      />
+                    </div>
+                  </div>
+                );
+              })}
             </div>
-            <div className="w-1/2 flex justify-center items-center gap-4 py-2">
-              <Input value={label.label} className="w-3/5 text-center " />
-              <Image
-                src="/assets/icons/delete-merge-item.svg"
-                alt="Delete"
-                width={12}
-                height={12}
-              ></Image>
-            </div>
-          </div>
-        ))}
+          );
+        })}
       </div>
     </div>
   );
