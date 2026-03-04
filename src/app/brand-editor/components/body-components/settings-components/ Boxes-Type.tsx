4b9e725481b1880b90a99553adac4902e4f97f03
florandres-latii <flor@latii.com>
2026-03-04 17:46:49 +0100

    boxes type

--- src/app/brand-editor/components/body-components/settings-components/ Boxes-Type.tsx
+++ src/app/brand-editor/components/body-components/settings-components/ Boxes-Type.tsx
@@ -1,8 +1,89 @@
 "use client";
 
 import Button from "@/components/Button";
-
+import Image from "next/image";
 const BoxesType = () => {
+  const boxes = [
+    {
+      name: "Item",
+      description: "Item box",
+      color: "blue",
+      search_prompt: "",
+      analysis_prompt: "",
+      is_deleted: false,
+      create_time: "2026-03-03T16:04:13",
+      update_time: "2026-03-03T16:04:13",
+      update_user: "yunlong@latii.com",
+      create_user: "yunlong@latii.com",
+      id: 1,
+    },
+    {
+      name: "Table",
+      description: "Table box",
+      color: "teal",
+      search_prompt: "",
+      analysis_prompt: "",
+      is_deleted: false,
+      create_time: "2026-03-03T16:04:28",
+      update_time: "2026-03-03T16:04:28",
+      update_user: "yunlong@latii.com",
+      create_user: "yunlong@latii.com",
+      id: 2,
+    },
+    {
+      name: "Context information",
+      description: "Context information box",
+      color: "",
+      search_prompt: "orange",
+      analysis_prompt: "",
+      is_deleted: false,
+      create_time: "2026-03-03T16:05:02",
+      update_time: "2026-03-03T16:05:02",
+      update_user: "yunlong@latii.com",
+      create_user: "yunlong@latii.com",
+      id: 3,
+    },
+    {
+      name: "Keynotes",
+      description: "Keynotes box",
+      color: "red",
+      search_prompt: "",
+      analysis_prompt: "",
+      is_deleted: false,
+      create_time: "2026-03-03T16:06:08",
+      update_time: "2026-03-03T16:06:08",
+      update_user: "yunlong@latii.com",
+      create_user: "yunlong@latii.com",
+      id: 4,
+    },
+    {
+      name: "test",
+      description: "",
+      color: "purple",
+      search_prompt: "123",
+      analysis_prompt: "456",
+      is_deleted: false,
+      create_time: "2026-03-04T10:37:12",
+      update_time: "2026-03-04T10:37:12",
+      update_user: "wangyuncong",
+      create_user: "wangyuncong",
+      id: 5,
+    },
+    {
+      name: "Box1",
+      description: "string",
+      color: "green",
+      search_prompt: "string",
+      analysis_prompt: "string",
+      is_deleted: false,
+      create_time: "2026-03-04T13:51:05",
+      update_time: "2026-03-04T13:51:05",
+      update_user: "flor@latii.com",
+      create_user: "flor@latii.com",
+      id: 7,
+    },
+  ];
+
   return (
     <div className="w-full h-full  text-xs pr-20">
       {/* titles */}
@@ -20,14 +101,34 @@ const BoxesType = () => {
           </Button>
         </div>
       </div>
-      <div>
+      <div className="w-full">
         {/* encabezados */}
-        <div className="w-full flex bg-primaryN20 border-b border-b-primaryN30 text-grey-basic">
+        <div className=" w-full flex bg-primaryN20 border-b border-b-primaryN30 text-grey-normal py-3 rounded-t-md ">
           <div className="w-2/12 text-center">Logic Name</div>
           <div className="w-1/12 text-center">Color</div>
           <div className="w-4/12 text-center">Search Prompt</div>
           <div className="w-5/12 text-center">Analysis Prompt</div>
         </div>
+        {/*  Lineas */}
+        {boxes?.map((box) => (
+          <div
+            key={`${box.id}-${box.name}`}
+            className="w-full flex py-6 border-b border-primaryN30"
+          >
+            <div className="w-2/12 text-center">{box.name}</div>
+            <div className="w-1/12 text-center">Color</div>
+            <div className="w-4/12 ">{box.search_prompt}</div>
+            <div className="w-5/12 flex space-between items-center">
+              <p className="w-11/12"> {box.analysis_prompt}</p>
+              <Image
+                src="/assets/icons/three-dots.svg"
+                alt=""
+                width={20}
+                height={10}
+              />
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
