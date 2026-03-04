28fb4a9d852aa3e1c30968867e3dcaba78c21a65
florandres-latii <flor@latii.com>
2026-03-04 15:27:56 +0100

    Boxes Type colors constants

--- src/app/brand-editor/components/body-components/settings-components/ Boxes-Type.tsx
+++ src/app/brand-editor/components/body-components/settings-components/ Boxes-Type.tsx
@@ -1,9 +1,34 @@
 "use client";
 
+import Button from "@/components/Button";
+
 const BoxesType = () => {
   return (
-    <div className="w-full h-full flex justify-center items-center">
-      Coming Soon
+    <div className="w-full h-full  text-xs pr-20">
+      {/* titles */}
+      <div className="flex justify-between items-center w-full mb-8">
+        <div className="flex flex-col gap-1">
+          <p className="text-base text-grey-base-dark">Box Information</p>
+          <p className="text-grey-normal">
+            This is the box information available for all takeoffs. Create up to
+            20 options.
+          </p>
+        </div>
+        <div>
+          <Button backgroundColor={"forumBlue-normal"} className="rounded-md">
+            + Add Box Type
+          </Button>
+        </div>
+      </div>
+      <div>
+        {/* encabezados */}
+        <div className="w-full flex bg-primaryN20 border-b border-b-primaryN30 text-grey-basic">
+          <div className="w-2/12 text-center">Logic Name</div>
+          <div className="w-1/12 text-center">Color</div>
+          <div className="w-4/12 text-center">Search Prompt</div>
+          <div className="w-5/12 text-center">Analysis Prompt</div>
+        </div>
+      </div>
     </div>
   );
 };
---
+++ src/lib/constants.ts
@@ -0,0 +1,16 @@
+const boxesColors = {
+  blue: "#3563A5", //forumBlue
+  green: "#009447", //accents
+  orange: "#FF931D",
+  yellow: "#F5C00B",
+  red: "#E83838",
+  cyan: "#0BC6BE",
+  teal: "#00798A",
+  violet: "#D868D8",
+  indigo: "#5859D6",
+  purple: "#8626B7",
+  brown: "#A3835F",
+  light_blue: "#23A6E1",
+  grey: "#A3A3A3",
+  dark_blue: "#284A7C",
+};
