1ea6c68458930e061da2a8ab43c556f3f55d8b53
Tomas Bran <tomas@latii.com>
2026-03-10 15:25:39 +0100

    fix(account settings page): changed the tabs to match figma design

--- src/app/account-settings/page.tsx
+++ src/app/account-settings/page.tsx
@@ -1,12 +1,12 @@
 "use client";
 
 import { useUser } from "@/context/UserContext";
-import { ConfigProvider, Tabs, TabsProps } from "antd";
+import { ConfigProvider } from "antd";
 
 import YourProfile from "./components/YourProfile";
 import TeamMembers from "./components/TeamMembers";
 import { useSearchParams } from "next/navigation";
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 
 const AccountSettings = () => {
   const { isAdmin } = useUser();
@@ -19,53 +19,61 @@ const AccountSettings = () => {
     return "1";
   }, [tab]);
 
-  const itemsAdmin: TabsProps["items"] = [
+  const tabs = [
     {
-      key: "1",
-      label: "Your Profile",
-      children: <YourProfile />,
-    },
-    {
-      key: "2",
-      label: "Team Management",
-      children: <TeamMembers />,
+      id: "1",
+      text: "Your Profile",
     },
   ];
 
-  const items: TabsProps["items"] = [
+  const adminTabs = [
     {
-      key: "1",
-      label: "Your Profile",
-      children: <YourProfile />,
+      id: "1",
+      text: "Your Profile",
+    },
+    {
+      id: "2",
+      text: "Team Management",
     },
   ];
+
+  const items = isAdmin ? adminTabs : tabs;
+
+  const [selectedTabId, setSelectedTabId] = useState<any>(defaultKey);
+
   return (
     <div className="mt-10 flex flex-col gap-2 w-full">
-      <div className="flex flex-col gap-1 pl-12">
+      <div className="flex flex-col gap-1 pl-12 border-b border-grey-light-hover pb-6 mb-4">
         <p className="text-xl text-forumBlue-normal">Account Preferences</p>
         <p className="text-xs text-grey-normal">
           Manage your data and your team members, privacy and security.
         </p>
       </div>
 
-      <ConfigProvider
-        theme={{
-          components: {
-            Tabs: {
-              inkBarColor: "#555555",
-              itemSelectedColor: "#555555",
-              itemColor: "#A3A3A3",
-              itemHoverColor: "#555555",
-            },
-          },
-        }}
-      >
-        <Tabs
-          className="[&_.ant-tabs-tab]:w-36 [&_.ant-tabs-tab]:justify-center [&_.ant-tabs-nav]:mb-0 [&_.ant-tabs-nav::before]:border-b-primaryN30 [&_.ant-tabs-nav::before]:!opacity-100 [&_.ant-tabs-nav-wrap]:pl-12"
-          defaultActiveKey={defaultKey}
-          items={isAdmin ? itemsAdmin : items}
-        />
-      </ConfigProvider>
+      <div className="flex pl-12 gap-2">
+        {items.map((item: any) => {
+          return (
+            <div
+              key={item.id}
+              onClick={() => setSelectedTabId(item.id)}
+              className={`flex items-center justify-center w-32 h-8 rounded-md cursor-pointer text-xs ${
+                item.id === selectedTabId
+                  ? "bg-forumBlue-light text-grey-dark font-semibold"
+                  : "bg-white text-grey-light-strong"
+              }`}
+            >
+              {item.text}
+            </div>
+          );
+        })}
+      </div>
+      <div className={selectedTabId === "1" ? "block" : "hidden"}>
+        <YourProfile />
+      </div>
+
+      <div className={selectedTabId === "2" ? "block" : "hidden"}>
+        <TeamMembers />
+      </div>
     </div>
   );
 };
