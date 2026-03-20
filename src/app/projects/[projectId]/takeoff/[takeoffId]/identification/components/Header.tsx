93ec59ee8a3d2115e8c62ac96bc7c9f034a24aef
miaoxingqiuqiu0020 <guona0020@gmail.com>
2026-03-20 10:41:37 +0800

    Fix file operation process page, when executing browser default back processing, the header section does not display the file

--- src/app/projects/[projectId]/takeoff/[takeoffId]/identification/components/Header.tsx
+++ src/app/projects/[projectId]/takeoff/[takeoffId]/identification/components/Header.tsx
@@ -91,7 +91,7 @@ const Header = ({
       console.log('导航类型:', navEntry.type);
       // 可能的值: "navigate" | "reload" | "back_forward" | "prerender"
 
-      if (navEntry.type === 'reload') {
+      if (navEntry.type === 'reload' || navEntry.type === 'back_forward') {
         // 页面刷新的话，takeoffContext中的数据会被重置，此时需要重新获取数据
         getTakeOffDetails();
       }
